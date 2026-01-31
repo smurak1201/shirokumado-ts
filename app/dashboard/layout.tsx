e63ceb12942ba56ea535aa426d5651bc5c4b7599
smurak1201 <s.murakoshi1201@gmail.com>
2026-01-31 17:47:02 +0900

    feat: ダッシュボードレイアウトの余白を調整

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -24,7 +24,7 @@ export default async function DashboardLayout({ children }: DashboardLayoutProps
 
   return (
     <div className="min-h-screen bg-gray-50">
-      <div className="py-8">
+      <div>
         <div className="mx-auto max-w-4xl px-4 sm:px-6 lg:px-8">
           <DashboardHeader
             title="ダッシュボード"
