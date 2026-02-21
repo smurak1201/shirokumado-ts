bc0e3333131ea049ed5e0cf8ebfd91b311abf193
smurak1201 <s.murakoshi1201@gmail.com>
2026-02-21 19:41:25 +0900

    フォント設定に"display: swap"を追加

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,6 +13,7 @@ const notoSansJP = Noto_Sans_JP({
   variable: "--font-noto-sans-jp",
   subsets: ["latin"],
   weight: ["300", "400", "500", "700"],
+  display: "swap",
 });
 
 export const metadata: Metadata = {
